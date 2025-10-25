780919734b8ff7f2b245a88a6fd40d8573876790
Max Taylor <max@cakerstream.com>
2025-10-25 17:09:06 +0100

    Added journey together

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         include_str!("../expansions/obsidian_flames.json"),
         include_str!("../expansions/temporal_forces.json"),
         include_str!("../expansions/surging_sparks.json"),
+        include_str!("../expansions/journey_together.json"),
         include_str!("../expansions/mega_evolution.json"),
     ];
 
